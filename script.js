a5acb397e21724e819e826279a53fc6b924d7bf0
Robert Strzelczyk <robertstrzelczyk98@gmail.com>
2023-03-29 19:24:56 +0200

    update

--- script.js
+++ script.js
@@ -1,125 +1,79 @@
 "use strict";
-const body = document.querySelector("body");
-const toggle = document.querySelector(".toggle");
-const input = document.getElementById("new-task");
-const item = document.getElementById("items");
+
 const list = document.querySelector(".task-list");
-const card = document.querySelector(".card");
 const toDoList = {};
 let taskCount = 0;
 let size = 0;
 
 window.addEventListener("load", function () {
-  taskCount = Number(this.localStorage.getItem("counter"));
-  for (const key in this.localStorage) {
-    if (key.includes("task"))
-      toDoList[key] = JSON.parse(this.localStorage[key]);
+  taskCount = Number(localStorage.getItem("counter"));
+  for (const key in localStorage) {
+    if (key.includes("task")) toDoList[key] = JSON.parse(localStorage[key]);
   }
   for (const key in toDoList) {
     createNewTask(toDoList[key]);
   }
   [...list.children].forEach((el) => {
-    if (el.children[2].classList.contains("completed")) {
+    if (el.children[2].classList.contains("completed"))
       el.children[0].checked = true;
-    }
   });
-  size = Object.keys(toDoList).length;
-  item.textContent = `${size} items left`;
-  if (size > 0) {
-    card.classList.remove("hidden");
-  }
+  itemsLeft();
+  displayTaskList();
   if (this.matchMedia && this.matchMedia("(prefers-color-scheme:dark)").matches)
-    body.classList.add("dark");
-});
-
-toggle.addEventListener("click", function (e) {
-  body.classList.contains("dark")
-    ? body.classList.remove("dark")
-    : body.classList.add("dark");
+    document.body.classList.add("dark");
 });
 
 document.addEventListener("keydown", function (e) {
-  if (e.key === "Enter") {
-    taskCount++;
-    localStorage.setItem(`counter`, taskCount);
+  const input = document.getElementById("new-task");
+  if (e.key === "Enter" && input.value != "") {
     const task = {
-      id: `task-${taskCount}`,
+      id: `task-${++taskCount}`,
       info: input.value,
       status: "active",
     };
     input.value = "";
-    input.blur();
     toDoList[`task-${taskCount}`] = task;
+    localStorage.setItem(`counter`, taskCount);
     localStorage.setItem(`task-${taskCount}`, JSON.stringify(task));
-    size = Object.keys(toDoList).length;
     createNewTask(task);
-    item.textContent = `${size} items left`;
-    if (size > 0) {
-      card.classList.remove("hidden");
-    }
-  }
+    itemsLeft();
+    displayTaskList();
+  } else return;
 });
 
 document.addEventListener("click", function (e) {
   if (e.target.closest(".checkbox")) {
     const target = e.target.closest("label");
-    if (
-      !target.parentElement.children[0].checked &&
-      target.parentElement.children[2].classList.contains("active")
-    ) {
-      target.parentElement.children[2].classList.remove("active");
-      target.parentElement.children[2].classList.add("completed");
-      toDoList[target.getAttribute("for")].status = "completed";
-      localStorage.setItem(
-        target.getAttribute("for"),
-        JSON.stringify(toDoList[target.getAttribute("for")])
-      );
-    } else if (
-      target.parentElement.children[0].checked &&
-      !target.parentElement.children[2].classList.contains("active")
-    ) {
-      target.parentElement.children[2].classList.remove("completed");
-      target.parentElement.children[2].classList.add("active");
-      toDoList[target.getAttribute("for")].status = "active";
-      localStorage.setItem(
-        target.getAttribute("for"),
-        JSON.stringify(toDoList[target.getAttribute("for")])
-      );
+    const checkbox = target.parentElement.children[0].checked;
+    const taskStatus = target.parentElement.children[2].classList;
+    if (!checkbox && taskStatus.contains("active")) {
+      taskStatus.remove("active");
+      taskStatus.add("completed");
+      updateStatus.call(target, "completed");
+    } else if (checkbox && taskStatus.contains("completed")) {
+      taskStatus.remove("completed");
+      taskStatus.add("active");
+      updateStatus.call(target, "active");
     }
   } else if (e.target.closest(".btn-clear")) {
     [...list.children].forEach((el) => {
-      if (!el.children[2].classList.contains("active")) {
-        e.target.closest(".btn-clear").blur();
-        delete toDoList[el.children[0].id];
-        localStorage.removeItem(el.children[0].id);
-        el.remove();
-        size = Object.keys(toDoList).length;
-        item.textContent = `${size} items left`;
-        if (size <= 0) {
-          card.classList.add("hidden");
-          taskCount = 0;
-          localStorage.clear();
-        }
-      }
+      if (el.children[2].classList.contains("active")) return;
+      delete toDoList[el.children[0].id];
+      localStorage.removeItem(el.children[0].id);
+      el.remove();
+      itemsLeft();
+      displayTaskList();
     });
   } else if (e.target.closest(".btn-active")) {
-    [...list.children].forEach((el) => {
-      el.classList.remove("hidden");
-      if (!el.children[2].classList.contains("active")) {
-        el.classList.add("hidden");
-      }
-    });
+    filterTasks("active");
   } else if (e.target.closest(".btn-completed")) {
-    [...list.children].forEach((el) => {
-      el.classList.remove("hidden");
-      if (el.children[2].classList.contains("active")) {
-        el.classList.add("hidden");
-      }
-    });
+    filterTasks("completed");
   } else if (e.target.closest(".btn-all")) {
-    [...list.children].forEach((el) => {
-      el.classList.remove("hidden");
-    });
+    filterTasks();
+  } else if (e.target.closest(".toggle")) {
+    document.body.classList.contains("dark")
+      ? document.body.classList.remove("dark")
+      : document.body.classList.add("dark");
   } else return;
 });
 
@@ -129,7 +83,7 @@ function createNewTask(data) {
           <input type="checkbox" name="task" id="${id}" />
           <label class="checkbox" for="${id}"></label>
           <p class="task-description ${status}">${info}</p>
-          <button class="btn-del" aria-label="Delete task" onclick="deleteTask.bind(this)()"></button>
+          <button class="btn-del" aria-label="Delete task" onclick="deleteTask.call(this)"></button>
         </div>`;
   list.insertAdjacentHTML("afterbegin", html);
 }
@@ -140,11 +94,39 @@ function deleteTask() {
   delete toDoList[taskId];
   localStorage.removeItem(taskId);
   task.remove();
+  itemsLeft();
+  displayTaskList();
+}
+
+function itemsLeft() {
+  const item = document.getElementById("items");
   size = Object.keys(toDoList).length;
   item.textContent = `${size} items left`;
-  if (size <= 0) {
+}
+
+function displayTaskList() {
+  const card = document.querySelector(".card");
+  if (size > 0) {
+    card.classList.remove("hidden");
+  } else if (size <= 0) {
     card.classList.add("hidden");
     taskCount = 0;
     localStorage.clear();
   }
+}
+
+function updateStatus(msg) {
+  toDoList[this.getAttribute("for")].status = msg;
+  localStorage.setItem(
+    this.getAttribute("for"),
+    JSON.stringify(toDoList[this.getAttribute("for")])
+  );
+}
+
+function filterTasks(value) {
+  [...list.children].forEach((el) => {
+    el.classList.remove("hidden");
+    if (value && !el.children[2].classList.contains(value))
+      el.classList.add("hidden");
+  });
 }
